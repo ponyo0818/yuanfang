dd816b47e32344f8c360ea0b4cda8d2992c68934
yfang309 <yfang309@gatech.edu>
2021-06-17 17:16:52 -0400

    Tweak it

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,7 +128,7 @@ module.exports = {
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
+          // optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           cookie_expires: 0,
         },
